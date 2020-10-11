368056f70ea03e5330fb0393f564a98bfea0835e
isharrma <54413011+isharrma@users.noreply.github.com>
2020-10-11 18:17:28 +0530

    Challenge 2

--- static/script.js
+++ static/script.js
@@ -1,10 +1,23 @@
 
 
-
 function ageInDays(){
-var birthyear = prompt("Which year were you born sweety?")
-var ageinDays = (2020 - birthyear) * 365
-var h1 = document.createElement('h1')
+     var birthyear = prompt("Which year were you born sweety?")
+     var ageinDays = (2020 - birthyear) * 365
+     var h2 = document.createElement('h2')
+     var textAnswer = document.createTextNode('You are '+ ageinDays + ' days old. ')
+     h2.setAttribute('id','ageinDays')
+     h2.appendChild(textAnswer)
+     document.getElementById('flex-box-result').appendChild(h2)
+}
 
+function reset(){
+     document.getElementById('ageinDays').remove()
+}
+//ageInDays()
 
+function catGenerator(){
+     var image = document.createElement('img')
+     var div = document.getElementById('cat-generator')
+     image.src = "C:/Users/DELL/Desktop/cat.gif"
+     div.appendChild(image)
 }
