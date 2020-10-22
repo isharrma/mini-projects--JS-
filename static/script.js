d6cef90b6b53583c1aee6dbf3f700e93dae20a09
isharrma <54413011+isharrma@users.noreply.github.com>
2020-10-22 17:09:24 +0530

    Almost Done

--- static/script.js
+++ static/script.js
@@ -155,18 +155,72 @@ function buttonRandom(){
 
 let blackjackGame = {
      'you': { 'scoreSpan': '#your-blackjack-result','div': '#your-box','score':0},
-     'dealer': {'scoreSpan':'#dealer-blackjack-result', 'div': '#dealer-box','score': 0}
+     'dealer': {'scoreSpan':'#dealer-blackjack-result', 'div': '#dealer-box','score': 0},
+     'cards' : ['2' , '3','4','5','6','7','8','9','10','K','J','Q','A'],
+     'cardsMap': {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'K':10,'J':10,'Q':10,'A':[1,11] }
 }
 
 const YOU = blackjackGame['you']
 const DEALER = blackjackGame['dealer']
 
+const hitSound = new Audio('static/sounds/swoosh.mp3')
 
 document.querySelector('#blackjack-hit-button').addEventListener('click',blackjackHit)
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackjackDeal)
 
 function blackjackHit(){
+     let card = randomCard()
+     showCard(card,YOU)
+     showCard(card,DEALER)
+     updateScore(card, DEALER )
+     blackjackyourScore = blackjackGame['dealer']['score']
+    showScore(DEALER)
+    
+}
+
+function randomCard(){
+     let randomIndex = Math.floor(Math.random()*13)
+     return blackjackGame['cards'][randomIndex]
+}
+
+function showCard(card,activePlayer){
      let cardImage = document.createElement('img')
-     cardImage.src = 'static/image/Q.png'
-     document.querySelector(YOU['div']).appendChild(cardImage)
-     
+     cardImage.src = 'static/image/' + card +'.png'
+     document.querySelector(activePlayer['div']).appendChild(cardImage)
+     hitSound.play()
+}
+
+function blackjackDeal(){
+     let yourImages = document.querySelector('#your-box').querySelectorAll('img')
+     let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img')
+
+     for(i=0; i < yourImages.length; i++){
+          yourImages[i].remove()
+     }
+
+     for(i=0; i < dealerImages.length; i++){
+          dealerImages[i].remove()
+     }
+     removeScore(DEALER)
+}
+
+function updateScore(card,activePlayer){
+     if(card =='A'){
+          if(activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21){
+               activePlayer['score'] += blackjackGame['cardsMap'][card][1]
+          }else{
+               activePlayer['score'] += blackjackGame['cardsMap'][card][0]
+          }
+     }
+
+     activePlayer['score'] += blackjackGame['cardsMap'][card]
+}
+
+function showScore(activePlayer){
+     document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score']
+}
+
+function removeScore(activePlayer){
+     activePlayer['score'] = 0 
+     document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score']
 }
